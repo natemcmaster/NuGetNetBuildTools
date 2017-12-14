739e5ec73745a2c5efe81312a537d35763842c29
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2017-12-14 15:09:43 +0000

    initial

---
+++ src/MyTypescriptGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyTypescriptGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] != null)
+                Console.WriteLine(args[0]);
+
+            if (args[1] != null)
+                Console.WriteLine(args[1]);
+        }
+    }
+}
